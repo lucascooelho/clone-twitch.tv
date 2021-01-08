8ab57b5f630e4e8b3f9dab8e3f8258ba03008d05
Lucas N. Coelho <lucascooelho@hotmail.com>
2021-01-08 15:28:35 -0300

    First commit

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import {
+  List,
+  ChannelContainer,
+  LeftSide,
+  Avatar,
+  Column,
+  Username,
+  Info,
+  RightSide,
+  WhiteCircle,
+} from "./styles";
+
+const ChannelList: React.FC = () => {
+  const ChannelItem = () => (
+    <ChannelContainer>
+      <LeftSide>
+        <Avatar />
+        <Column>
+          <Username>devsoficial</Username>
+          <Info>17 new videos</Info>
+        </Column>
+      </LeftSide>
+
+      <RightSide>
+        <WhiteCircle />
+      </RightSide>
+    </ChannelContainer>
+  );
+  return (
+    <List>
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+    </List>
+  );
+};
+
+export default ChannelList;
